499f81a3f2c1d1b252283c83420feea0a4b15cfc
damianTrybuszek <damian.trybuszek@gmail.com>
2021-09-18 16:01:59 +0200

    2nd task completed.

--- src/test/java/com/codecool/histogram/TextReaderTest.java
+++ src/test/java/com/codecool/histogram/TextReaderTest.java
@@ -8,5 +8,42 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TextReaderTest {
+
+
+
+    @Test
+    public void whenReadTextWithOneLine_ReturnCorrectString() throws IOException {
+        TextReader textReader = new TextReader("src/test/resources/test.txt");
+        String result = textReader.read() ;
+        assertEquals("Harry Potter and the Sorcerer's Stone" + System.lineSeparator(), result);
+    }
+
+    @Test
+    public void whenFileNotExist_ThrowFileNotFoundException() {
+        TextReader textReader = new TextReader("src/test/resources/test12");
+        assertThrows(FileNotFoundException.class, textReader::read
+        );
+    }
+
+    @Test
+    public void whenFileIsEmpty_ReturnEmptyString() throws IOException {
+        TextReader textReader = new TextReader("src/test/resources/empty.txt");
+        String result = textReader.read() ;
+        assertEquals("", result);
+    }
+
+    @Test
+    public void whenFileHasManyLines_ReturnCorrectNumberOfLines() throws IOException {
+        TextReader textReader = new TextReader("src/test/resources/text.txt");
+        String result = textReader.read() ;
+        String [] lines = result.split(System.lineSeparator());
+        assertEquals(33, lines.length);
+    }
+
+
+
+
+
+
     
 }
